1bbbf58033f3670c8d4f8e40d9d0e58b213918a0
Siderea <hutornoyaa@gmail.com>
2023-12-11 19:59:06 +0300

    addburgers

--- main.js
+++ main.js
@@ -237,3 +237,15 @@ $(function() {
 	}
 	return false;
 });
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
